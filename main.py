7384e114f8fd7b8d245af368d1c7fa40a0759ae8
Tomás <tomasmitsuo@gmail.com>
2023-05-05 21:20:49 -0300

    Loop epasta criada

--- main.py
+++ main.py
@@ -20,23 +20,35 @@ if __name__ == '__main__':
   videoLength = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
   fps = int(video.get(cv2.CAP_PROP_FPS))
 
-  frameNumber = (requestedFrameNumber / (videoLength * fps))
+  # Cria um novo Diretório com o videoDestinationPath (Por o nome da Pessoa)
 
-  if frameNumber > videoLength:
-    raise Exception('requested frame is longer than video.')
+  current_directory = os.getcwd()
+  final_directory = os.path.join(current_directory, videoDestinationPath)
+  if not os.path.exists(final_directory):
+    os.makedirs(final_directory)
 
-  # 1 is the prop for frame position in cv2 in python 3
-  video.set(1, frameNumber)
-  success, image = video.read()
+  for i in range(20):
 
-  if success == False:
-    raise Exception('failed to read the frame.')
+    frameNumber = (requestedFrameNumber / (videoLength * fps))   # Formula do numero do frame
 
+    if frameNumber > videoLength:
+      raise Exception('requested frame is longer than video.')
 
-  dest = os.path.realpath(os.getcwd() + '/assets/output')
-  if videoDestinationPath:
-    dest = os.path.abspath(videoDestinationPath)
+    # 1 is the prop for frame position in cv2 in python 3
 
-  cv2.imwrite(dest + '/frame-%d.jpg' % requestedFrameNumber, image)
+    video.set(1, frameNumber)
+    success, image = video.read()
+
+    if success == False:
+      raise Exception('failed to read the frame.')
+
+    dest = os.path.realpath(os.getcwd() + '/assets/output')
+
+    if videoDestinationPath:
+      dest = os.path.abspath(videoDestinationPath)
+
+    cv2.imwrite(dest + '/frame-%d.jpg' % requestedFrameNumber, image)
+
+    requestedFrameNumber = requestedFrameNumber + 10000  #Atualiza o frame
 
   video.release()
